0b9fcfcac1e6a3fbddded469cb7b6fe3c568fdc9
Logan-Fullerton <144399401+Logan-Fullerton@users.noreply.github.com>
2024-05-07 18:36:13 -0700

    made portfolio page size responsive

--- src/pages/portfolio.jsx
+++ src/pages/portfolio.jsx
@@ -1,36 +1,44 @@
+import React from 'react';
 
-
-function Portfolio(){
-    return(
-        <div class="container text-center">
-        <div class="row align-items-start">
-          <div class="col">
-            Expense Tracker
-            <div>
-                <a href='#'> 
-                <img src="src\assets\Expense Tracker - Google Chrome 1_10_2024 7_31_38 PM.png" alt="placeholder" class ='img-fluid'/>
-                </a>
+function Portfolio() {
+    return (
+        <div className="container text-center">
+            <div className="row">
+                <div className="col">
+                    <h1>My Project Portfolio</h1>
+                </div>
             </div>
-          </div>
-          <div class="col">
-            Stock Market Dashboard
-            <div>
-                <a href='#'> 
-                <img src="src\assets\Stocks Dashboard - Google Chrome 11_7_2023 5_50_11 PM.png" alt="placeholder" class ='img-fluid'/>
-                </a>
+            <div className="row">
+                <div className="col-lg-4 col-md-6 col-sm-12">
+                    <h2 className="bg-dark text-light">Expense Tracker</h2>
+                    <div>
+                        <a href='#'>
+                            <img src="src\assets\expense-tracker.jpg" alt="Expense Tracker" className='img-fluid' />
+                        </a>
+                    </div>
+                </div>
+                <div className="col-lg-4 col-md-6 col-sm-12">
+                    <h2 className="bg-dark text-light">Stock Market Dashboard</h2>
+                    <div>
+                        <a href='#'>
+                            <img src="src\assets\stock-market-dashboard.jpg" alt="Stock Market Dashboard" className='img-fluid' />
+                        </a>
+                    </div>
+                </div>
+                <div className="col-lg-4 col-md-6 col-sm-12">
+                    <h2 className="bg-dark text-light">EFT Ballistics</h2>
+                    <div>
+                        <a href='#'>
+                            <img src="src\assets\eft.png" alt="EFT Ballistics" className='img-fluid' />
+                        </a>
+                    </div>
+                </div>
             </div>
-          </div>
-          <div class="col">
-            EFT Ballistics
-            <div>
-                <a href='#'> 
-                <img src="https://via.placeholder.com/150" alt="placeholder" class ='img-fluid' />
-                </a>
-            </div>
-          </div>
         </div>
-      </div>
-        
-    )
+    );
 }
-export default Portfolio
+
+export default Portfolio;
+
+
+
