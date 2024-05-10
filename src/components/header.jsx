e5ec255143d6a031d874dcd17847ace4c5a039a1
Logan-Fullerton <144399401+Logan-Fullerton@users.noreply.github.com>
2024-05-09 19:03:52 -0700

    styling

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -1,14 +1,14 @@
 export default function Header(){
     return (
         <header>
-           <nav class="navbar navbar-expand-lg bg-body-tertiary">
+           <nav className="navbar navbar-expand-md " style={{background:"#D1D1B1"}}>
   <div className="container">
     <a className="navbar-brand" href="/">Logan Fullerton</a>
     <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
       <span className="navbar-toggler-icon"></span>
     </button>
-    <div className="collapse navbar-collapse" id="navbarSupportedContent">
-      <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+    <div className="navbar-collapse" id="navbarSupportedContent">
+      <ul className="navbar-nav ms-auto">
         <li className="nav-item">
           <a className="nav-link" href="contact">Contact</a>
         </li>
--- src/pages/about.jsx
+++ src/pages/about.jsx
@@ -1,13 +1,25 @@
+import React from 'react'
+
 function About(){
     return(
-        <div className="container">
-            <div className="container">
+        <div className="container mt-4" style={{minHeight: "100vh",background:"#BABAAA"}}>
+            <div className="col pt-4 ">
                 <img src="https://via.placeholder.com/150" alt="placeholder" />
                 </div>
             <div  className="mt-4 mb-4 text-center" >
                 <h1>About Me</h1>
                 <p>
-                    I am a full stack web developer with a passion for creating and designing web applications
+                    I am a full stack JavaScript developer with a passion for creating. My interest in web development stems from my background in metal fabrication
+                    and design, as well as my love for building computers. I have always enjoyed the process of creating something cool from nothing, and I discovered 
+                    that web development is a great way to express myself creatively while also having the potential to solve real world problems. I graduated from the Full Stack 
+                    Bootcamp at the university of California Davis in 2023, and I am excited to continue my journey as a web developer.
+                </p>
+            </div>
+
+            <div  className="mt-4 mb-4 text-center" >
+                <h1>Front End Proficiencies </h1>
+                <p>
+                    
                 </p>
             </div>
 
